b8bd7fadcb8794663b472d5e443872ea3f292f6b
unknown <errational_richard@yahoo.com>
2022-07-05 16:00:30 -0700

    commit3

--- src/components/Movie/movie.js
+++ src/components/Movie/movie.js
@@ -6,35 +6,36 @@ export default class Movie extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-        movies:props.movies,
-        title:props.title,
+        title: props.title,
         synopsis: props.synopsis,
         rating: props.rating,
-        image:props.image,
-        time:props.time
-        }
+        image: props.image,
+       
+        };
     }
+     
+
+
   render() {
 
-    
-
-   
+    console.log("test",this.state.title)
+   console.log("image", this.state.image)
 
     return (
         
-        <div className="card ">
-        <img class="card-img-top" src="/images/starwars.jpg" alt="Card image cap"></img>
+        <div className="card " >
+        <img className="card-img-top" src={this.state.image} alt="Card image cap"></img>
         <div className='card-header bg-success text-white'>
           Movie Title
         </div>
 
         <div className='card-body'>
+          
           <h5 className= "card-title">{this.state.title}</h5>
           <p className= "card-text"> {this.state.synopsis}</p>
         </div>
         <ul className= "list-group list-group-flush">
             <li className="list-group-item">{this.state.rating}</li>
-            <li className="list-group-item">{this.state.time}</li>
         </ul>
         <div className='card-footer'>
           {/*<Star />
